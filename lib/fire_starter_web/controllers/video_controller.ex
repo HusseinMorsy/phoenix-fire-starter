96dc78727a61109410027148827d1486112d20cf
Hussein Morsy <hussein@morsy.de>
2018-02-02 18:06:41 +0100

    Refactor video controller k	modified:   lib/fire_starter_web/router.ex

--- lib/fire_starter_web/controllers/video_controller.ex
+++ lib/fire_starter_web/controllers/video_controller.ex
@@ -1,8 +1,9 @@
 defmodule FireStarterWeb.VideoController do
   use FireStarterWeb, :controller
+  alias FireStarter.{Repo, Video}
 
   def index(conn, _params) do
-    videos = FireStarter.Repo.all(FireStarter.Video)
+    videos = Repo.all(Video)
     render conn, "index.html", videos: videos
   end
 end
